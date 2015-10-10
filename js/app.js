eb677183947d7fa9f15d2744d0c1d8dc9634e5c1
zakharek <ekaterina.zhr@gmail.com>
2015-10-10 20:02:35 +1100

    Add player class

--- js/app.js
+++ js/app.js
@@ -2,24 +2,30 @@ Math.randomBetween = function(min, max){
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-// Enemies our player must avoid
-var Enemy = function(rowNumber) {
+var Enemy = function(row) {
+	//TODO: move all hardcoded values to configuration object
     var imageWidth = 101;
-    
-    this.x = -imageWidth;
-    this.y = this.getY(rowNumber);
-    this.row = rowNumber;
-    this.width = imageWidth;
-    this.speed = Math.randomBetween(150, 400);;
     this.sprite = 'images/enemy-bug.png';
+
+    this.x = -imageWidth;
+    this.y = this.getY(row);
+    this.row = row;
+    this.width = imageWidth;
+    this.speed = this.getRandomSpeed();
 };
 
-Enemy.prototype.getY = function(rowNumber) {
+Enemy.prototype.getRandomSpeed = function(){
+	//TODO: move all hardcoded values to configuration object
+	return Math.randomBetween(150, 400);
+}
+
+Enemy.prototype.getY = function(row) {
+	//TODO: move all hardcoded values to configuration object
 	var imageHeight = 80,
 		rowHeight = 83,
     	rowHightAdjustment = 60;
 
-    return (rowHeight/2 - imageHeight/2) + rowHeight * (rowNumber - 1) + rowHightAdjustment;
+    return (rowHeight/2 - imageHeight/2) + rowHeight * (row - 1) + rowHightAdjustment;
 };
 
 Enemy.prototype.update = function(dt) {
@@ -28,34 +34,76 @@ Enemy.prototype.update = function(dt) {
 
     this.x += xIncrement;
 
-    if(this.x > ctx.canvas.width) this.reset();
-};
-
-Enemy.prototype.reset = function() {
-	var rowNumber,
-		rowNumberIsSameAsAllOtherEnemiesHave,
-		enemiesOnSameRow,
-		speed = Math.randomBetween(150, 400);
-    
-	do {
-		rowNumber = Math.floor(Math.randomBetween(1, 3));
-		enemiesOnSameRow = allEnemies.filter(function(enemy){ return enemy.row === rowNumber; }).length;
-		rowNumberIsSameAsAllOtherEnemiesHave = enemiesOnSameRow === allEnemies.length;
-	} while(rowNumberIsSameAsAllOtherEnemiesHave)
-
-	this.x = -this.width;
-    this.y = this.getY(rowNumber);
-    this.row = rowNumber;
-    this.speed = speed;
+    var enemyLeftScreen = this.x > ctx.canvas.width;
+    if(enemyLeftScreen) this.reset();
 };
 
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Enemy.prototype.reset = function() {
+	var row,
+		enemiesOnSameRow,
+		allEnemiesWillBeOnSameRow;
+    
+	do {
+		//TODO: move all hardcoded values to configuration object
+		row = Math.floor(Math.randomBetween(1, 3));
+
+		enemiesOnSameRow = allEnemies.filter(function(enemy){ return enemy.row === row; }).length;
+		allEnemiesWillBeOnSameRow = (enemiesOnSameRow + 1) === allEnemies.length;
+	} while (allEnemiesWillBeOnSameRow);
+
+	this.x = -this.width;
+    this.y = this.getY(row);
+    this.row = row;
+    this.speed = this.getRandomSpeed();
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+var Player = function(column, row) {
+	//TODO: move all hardcoded values to configuration object
+    var imageWidth = 101;
+    
+    //TODO: move all hardcoded values to configuration object
+    this.sprite = 'images/char-boy.png';
+    this.x = this.getX(column);
+    this.y = this.getY(row);
+    this.column = column;
+    this.row = row;
+    this.width = imageWidth;
+};
+
+Player.prototype.getX = function(column){
+	//TODO: move all hardcoded values to configuration object
+	var imageWidth = 101,
+		columnWidth = 101;
+
+    return (columnWidth/2 - imageWidth/2) + columnWidth * (column - 1);
+}
+
+Player.prototype.getY = function(row){
+	//TODO: move all hardcoded values to configuration object
+	var imageHeight = 171,
+		rowHeight = 83,
+    	rowHightAdjustment = 10;
+
+    return (rowHeight/2 - imageHeight/2) + rowHeight * (row - 1) + rowHightAdjustment;
+}
+
+Player.prototype.update = function() {
+    
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
 
 
 // Now instantiate your objects.
@@ -63,6 +111,7 @@ Enemy.prototype.render = function() {
 // Place the player object in a variable called player
 
 var allEnemies = [];
+var player = new Player(3,6);
 
 (function () {
 
